ff8956c22248354fc5c7974baced0b19136d50c8
Hosam124 <hosamelellity121@gmail.com>
2026-04-17 21:18:32 +0200

    create CompanyController

---
+++ src/main/java/com/elleithy/job_portal_backend/company/controller/CompanyController.java
@@ -0,0 +1,9 @@
+package com.elleithy.job_portal_backend.company.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/companies")
+public class CompanyController {
+}
