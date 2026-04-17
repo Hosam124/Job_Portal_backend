c5a3bdf15a404a4c36f1a71f46b4f8df44f75b4a
Hosam124 <hosamelellity121@gmail.com>
2026-04-17 20:58:34 +0200

    init commit

--- src/main/java/com/elleithy/job_portal_backend/JobPortalBackendApplication.java
+++ src/main/java/com/elleithy/job_portal_backend/JobPortalBackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JobPortalBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JobPortalBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JobPortalBackendApplication.class, args);
+    }
 
 }
